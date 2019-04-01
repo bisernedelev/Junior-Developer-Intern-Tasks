3e1b109f0518abd849049b72d00150527fd53e35
bibo <bibo0088@abv.bg>
2019-04-01 13:19:45 +0300

    Junior Developer Intern Tasks 01.04.2019

---
+++ src/Task1_Factorial.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Task1_Factorial {
+
+    //Calculate factorial of a given number by the formula   n! = n × (n−1)!
+
+    public void calculateFactorial(int n) {
+        if (n != 0) { //guarantee that the program will catch condition when the input is "0"
+            int a = 1;
+            int secondFactorial = 1;
+            int mainResult;
+
+            for (int i = 0; i < n - 1; i++) {
+
+                secondFactorial *= a;
+                a++;
+            }
+            mainResult = n * secondFactorial;
+            System.out.println(mainResult);
+        } else {
+            System.out.println("0");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Task1_Factorial factorialCalculaton = new Task1_Factorial();
+        factorialCalculaton.calculateFactorial(sc.nextInt());
+
+    }
+}
